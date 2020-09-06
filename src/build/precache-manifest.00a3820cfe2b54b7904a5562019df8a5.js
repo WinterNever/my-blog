dadc03d0a0157d88a8ab3aa86f270661d36ff2a7
WinterNever <ramiro.marroquin.osoy@gmail.com>
2020-09-06 07:13:29 -0600

    first commit

---
+++ src/build/precache-manifest.00a3820cfe2b54b7904a5562019df8a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bd6b5a70294fdd08b3ee494a925640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f5aa04905d365754b5",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "3081974b0c48aac82ceb",
+    "url": "/static/js/2.962244cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.962244cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f5aa04905d365754b5",
+    "url": "/static/js/main.06aa8d69.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
